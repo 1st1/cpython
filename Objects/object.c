d4ec491c6ae95d6d3e0e031e0296dad6e8c3553b
Yury Selivanov <yury@magic.io>
2017-08-01 19:13:53 -0400

    hamt: boilerplate

--- Objects/object.c
+++ Objects/object.c
@@ -1684,6 +1684,9 @@ _Py_ReadyTypes(void)
     if (PyType_Ready(&PyDict_Type) < 0)
         Py_FatalError("Can't initialize dict type");
 
+    if (PyType_Ready(&PyHamt_Type) < 0)
+        Py_FatalError("Can't initialize hamt type");
+
     if (PyType_Ready(&PyDictKeys_Type) < 0)
         Py_FatalError("Can't initialize dict keys type");
 
