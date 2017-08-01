36c8619ab747da612c0a51e083e0b9ad43c8c583
Yury Selivanov <yury@magic.io>
2017-08-01 19:52:45 -0400

    hamt: layout node structs

--- Objects/hamtobject.c
+++ Objects/hamtobject.c
@@ -1,9 +1,43 @@
 #include "Python.h"
 
 
+static PyHamtNode_Bitmap *
+hamt_new_node_bitmap()
+{
+
+}
+
+
+static int
+hamt_node_clear(_PyHamtNode_BaseNode *self)
+{
+    return 0;
+}
+
+
+static int
+hamt_node_traverse(_PyHamtNode_BaseNode *self, visitproc visit, void *arg)
+{
+    return 0;
+}
+
+
+static void
+hamt_node_dealloc(PyObject *self)
+{
+    PyObject_GC_UnTrack(self);
+    (void)hamt_node_clear((_PyHamtNode_BaseNode*)self);
+    Py_TYPE(self)->tp_free(self);
+}
+
+
+///////////////////////////////////
+
+
 static int
 hamt_clear(PyHamtObject *self)
 {
+    Py_CLEAR(self->h_root);
     return 0;
 }
 
@@ -11,6 +45,7 @@ hamt_clear(PyHamtObject *self)
 static int
 hamt_traverse(PyHamtObject *self, visitproc visit, void *arg)
 {
+    Py_VISIT(self->h_root);
     return 0;
 }
 
@@ -28,6 +63,8 @@ hamt_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
     }
 
     h = (PyHamtObject *)self;
+    h->h_count = 0;
+    h->h_root = NULL;
     return self;
 }
 
@@ -36,9 +73,12 @@ static void
 hamt_dealloc(PyObject *self)
 {
     PyObject_GC_UnTrack(self);
-    (void)hamt_clear(self);
+    (void)hamt_clear((PyHamtObject*)self);
     Py_TYPE(self)->tp_free(self);
 }
+
+
+///////////////////////////////////
 
 
 PyTypeObject PyHamt_Type = {
@@ -52,3 +92,30 @@ PyTypeObject PyHamt_Type = {
     .tp_clear = (inquiry)hamt_clear,
     .tp_new = hamt_new,
 };
+
+
+#define DEFINE_NODE_TYPE(name)                                  \
+    PyTypeObject _PyHamt_##name##Node_Type = {                  \
+        PyVarObject_HEAD_INIT(&PyType_Type, 0)                  \
+        #name,                                                  \
+        sizeof(PyHamtNode_##name),                              \
+        .tp_dealloc = (destructor)hamt_node_dealloc,            \
+        .tp_getattro = PyObject_GenericGetAttr,                 \
+        .tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_HAVE_GC,    \
+        .tp_traverse = (traverseproc)hamt_node_traverse,        \
+        .tp_clear = (inquiry)hamt_node_clear,
+
+#define END_NODE_TYPE                                           \
+    }
+
+
+DEFINE_NODE_TYPE(Array)
+END_NODE_TYPE;
+
+
+DEFINE_NODE_TYPE(Bitmap)
+END_NODE_TYPE;
+
+
+DEFINE_NODE_TYPE(Collision)
+END_NODE_TYPE;
