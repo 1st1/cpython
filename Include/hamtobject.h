74bbf7facd9cc2762b4c7bdb816260c4ecbda815
Yury Selivanov <yury@magic.io>
2017-12-19 19:58:27 -0500

    PyHamtNode_Array is no longer a VarObject; dropped hamt_node_t.

--- Include/hamtobject.h
+++ Include/hamtobject.h
@@ -5,37 +5,29 @@ extern "C" {
 #endif
 
 
-typedef enum {Array, Bitmap, Collision} hamt_node_t;
-
-
 #define _PyHamtNode_Array_size 32
 
 
-#define _PyHAMT_HEAD(prefix)            \
-    PyObject_VAR_HEAD                   \
-    hamt_node_t prefix##_type;
-
-
 typedef struct {
-    _PyHAMT_HEAD(base)
+    PyObject_HEAD
 } _PyHamtNode_BaseNode;
 
 
 typedef struct {
-    _PyHAMT_HEAD(a)
+    PyObject_HEAD
     _PyHamtNode_BaseNode *a_array[_PyHamtNode_Array_size];
 } PyHamtNode_Array;
 
 
 typedef struct {
-    _PyHAMT_HEAD(b)
+    PyObject_VAR_HEAD
     uint32_t b_bitmap;
     PyObject *b_array[1];
 } PyHamtNode_Bitmap;
 
 
 typedef struct {
-    _PyHAMT_HEAD(c)
+    PyObject_VAR_HEAD
     int32_t c_hash;
     PyObject *c_array[1];
 } PyHamtNode_Collision;
@@ -48,16 +40,10 @@ typedef struct {
 } PyHamtObject;
 
 
-#define PyHamt_NodeType(node) (((_PyHamtNode_BaseNode*)(node))->base_type)
-
-
 PyAPI_DATA(PyTypeObject) PyHamt_Type;
 PyAPI_DATA(PyTypeObject) _PyHamt_ArrayNode_Type;
 PyAPI_DATA(PyTypeObject) _PyHamt_BitmapNode_Type;
 PyAPI_DATA(PyTypeObject) _PyHamt_CollisionNode_Type;
-
-
-#define PyHamt_CheckExact(op) (Py_TYPE(op) == &PyHamt_Type)
 
 
 #ifdef __cplusplus
